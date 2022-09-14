8981e70c1575c4a6ab16b3cc8c2f19f80b291279
Lucas Pires <lplucas355@gmail.com>
2022-09-14 12:31:41 -0300

    imc

---
+++ imc2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace imc2 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite seu peso : ");
+            double peso = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("digite sua altura : ");
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double imc = peso / (altura * altura);
+
+            if (imc >= 18.5 && imc <= 24.9) {
+                Console.WriteLine(" Seu IMC está normal : " + imc.ToString("f2", CultureInfo.InvariantCulture));
+            }
+            else if (imc >= 25.0 && imc <= 29.9) {
+                Console.WriteLine(" Seu IMC é Sobrepeso : " + imc.ToString("f2", CultureInfo.InvariantCulture));
+            }
+            else if (imc >= 30.0 && imc <= 39.0) {
+                Console.WriteLine(" Seu IMC é Obesidade : " + imc.ToString("f2", CultureInfo.InvariantCulture));
+            }
+            else {
+                Console.WriteLine(" Seu IMC é Obesidade Grave : " + imc.ToString("f2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
